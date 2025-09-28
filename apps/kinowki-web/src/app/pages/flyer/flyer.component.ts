f28bed3bc308b023b1f12d38d96b1f2bcecb9ed9
matwes <mateusz.jan.wesolowski@gmail.com>
2025-09-29 00:03:13 +0200

    fix flyer component

--- apps/kinowki-web/src/app/pages/flyer/flyer.component.ts
+++ apps/kinowki-web/src/app/pages/flyer/flyer.component.ts
@@ -1,8 +1,20 @@
-import { AfterViewInit, Component, effect, ElementRef, input, QueryList, signal, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  input,
+  QueryList,
+  signal,
+  ViewChildren,
+} from '@angular/core';
 import { FlyerDto } from '@kinowki/shared';
 import { Image, ImageModule } from 'primeng/image';
 
 import { FlyerNotePipe } from '../../utils';
+
+const OBJECT_STORAGE = 'https://pub-8a8f04577dee47a2a13d9a52562d4c46.r2.dev';
 
 @Component({
   selector: 'app-flyer',
@@ -15,7 +27,19 @@ export class FlyerComponent implements AfterViewInit {
   big = input(false);
   blankWidth = signal(175);
 
-  objectStorage = 'https://pub-8a8f04577dee47a2a13d9a52562d4c46.r2.dev/';
+  images = computed(() =>
+    this.flyer().images.map((image) => {
+      if (image.original && image.original !== 'blank') {
+        image.original = `${OBJECT_STORAGE}/${image.original}`;
+      }
+      if (image.thumbnail) {
+        image.thumbnail = `${OBJECT_STORAGE}/${image.thumbnail}`;
+      } else {
+        image.thumbnail = image.original;
+      }
+      return image;
+    })
+  );
 
   @ViewChildren(Image, { read: ElementRef }) imageEls!: QueryList<ElementRef<HTMLElement>>;
 
