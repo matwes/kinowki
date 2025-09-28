54e937642427521a3126674d3eb91a2ea909b65e
matwes <mateusz.jan.wesolowski@gmail.com>
2025-09-28 23:17:24 +0200

    enable cors

--- apps/kinowki-api/src/main.ts
+++ apps/kinowki-api/src/main.ts
@@ -10,8 +10,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: [
+      'https://kinowki.vercel.app',
+      'http://localhost:4200',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
