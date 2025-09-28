b6ae55e2c8c2a07bf3a7206cd8f822ec8de4073f
matwes <mateusz.jan.wesolowski@gmail.com>
2025-09-28 19:42:04 +0200

    disable serving static files

--- apps/kinowki-api/src/app.module.ts
+++ apps/kinowki-api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { FilmModule } from './film/film.module';
 import { ReleaseModule } from './release/release.module';
@@ -9,12 +9,12 @@ import { FlyerModule } from './flyer/flyer.module';
 import { TagModule } from './tag/tag.module';
 import { UserModule } from './user/user.module';
 import { UserFlyerModule } from './user-flyer/user-flyer.module';
-import { join } from 'path';
+// import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL, { dbName: 'kinowki' }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '../../../../flyers'), serveRoot: '/api/flyers' }),
+    // ServeStaticModule.forRoot({ rootPath: join(__dirname, '../../../../flyers'), serveRoot: '/api/flyers' }),
     DistributorModule,
     FilmModule,
     FlyerModule,
