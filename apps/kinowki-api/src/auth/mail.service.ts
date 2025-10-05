72e440273742dff5d52cf0e0e1acaad77a79d882
matwes <mateusz.jan.wesolowski@gmail.com>
2025-10-05 22:54:22 +0200

    fix smtp configuration

--- apps/kinowki-api/src/auth/mail.service.ts
+++ apps/kinowki-api/src/auth/mail.service.ts
@@ -4,7 +4,9 @@ import * as nodemailer from 'nodemailer';
 @Injectable()
 export class MailService {
   private transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
