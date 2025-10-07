511a6defe0c5477a0a09a08992e5411fb38a94df
matwes <mateusz.jan.wesolowski@gmail.com>
2025-10-07 22:52:03 +0200

    change image url

--- apps/kinowki-web/src/app/pages/flyer/flyer.component.ts
+++ apps/kinowki-web/src/app/pages/flyer/flyer.component.ts
@@ -12,9 +12,8 @@ import {
 import { FlyerDto } from '@kinowki/shared';
 import { Image, ImageModule } from 'primeng/image';
 
+import { environment } from '../../../environments/environment';
 import { FlyerNotePipe } from '../../utils';
-
-const OBJECT_STORAGE = 'https://pub-8a8f04577dee47a2a13d9a52562d4c46.r2.dev';
 
 @Component({
   selector: 'app-flyer',
@@ -23,6 +22,8 @@ const OBJECT_STORAGE = 'https://pub-8a8f04577dee47a2a13d9a52562d4c46.r2.dev';
   imports: [FlyerNotePipe, ImageModule],
 })
 export class FlyerComponent implements AfterViewInit {
+  private readonly CDN_URL = environment.cdnUrl;
+
   flyer = input.required<FlyerDto>();
   big = input(false);
   blankWidth = signal(175);
@@ -32,10 +33,10 @@ export class FlyerComponent implements AfterViewInit {
       const result = { ...image };
 
       if (result.original && result.original !== 'blank') {
-        result.original = `${OBJECT_STORAGE}${result.original}`;
+        result.original = `${this.CDN_URL}${result.original}`;
       }
       if (result.thumbnail) {
-        result.thumbnail = `${OBJECT_STORAGE}${result.thumbnail}`;
+        result.thumbnail = `${this.CDN_URL}${result.thumbnail}`;
       }
       return result;
     })
@@ -45,8 +46,7 @@ export class FlyerComponent implements AfterViewInit {
 
   constructor() {
     effect(() => {
-      const flyerValue = this.flyer();
-      if (flyerValue) {
+      if (this.flyer()) {
         queueMicrotask(() => this.updateBlankWidth());
       }
     });
@@ -58,7 +58,9 @@ export class FlyerComponent implements AfterViewInit {
 
   private updateBlankWidth(): void {
     const firstImageEl = this.imageEls.first?.nativeElement.querySelector('img');
-    if (!firstImageEl) return;
+    if (!firstImageEl) {
+      return;
+    }
 
     const setWidth = () => this.blankWidth.set(firstImageEl.offsetWidth);
 
